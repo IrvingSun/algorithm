f314ae23d6d792cc2138131f4af938effc41ac31
sunway <sunwei.pyw@gmail.com>
2022-12-29 20:20:42 +0800

    修改项目结构

---
+++ src/main/java/cn/sunway/algorithm/Test.java
@@ -0,0 +1,11 @@
+package cn.sunway.algorithm;
+
+/**
+ * @author sunw
+ * @date 2022/12/29
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("---1---1--1-1-1-1-1");
+    }
+}
