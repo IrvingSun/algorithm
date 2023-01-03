6206d2ff23ee736ace1b8ed0ea08f52b49eca83a
sunway <sunwei.pyw@gmail.com>
2023-01-03 23:13:16 +0800

    尾部添加node

--- src/main/java/cn/sunway/structure/ListNode.java
+++ src/main/java/cn/sunway/structure/ListNode.java
@@ -19,10 +19,30 @@ public class ListNode {
     }
 
     public void addTail(ListNode item){
-        ListNode tail = null;
+        ListNode tail = this;
         //找到最后一个节点
         while (this.next != null){
             tail = this.next;
         }
+        tail.next = item;
+    }
+
+    @Override
+    public String toString() {
+        String str = "["+this.data+"]";
+        ListNode current = this;
+        while(current.next!=null){
+            str += " --> [" + this.next.data +"]";
+            current = this.next;
+        }
+        str += " -->NULL";
+        return str;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        node1.addTail(node2);
+        System.out.println(node1);
     }
 }
