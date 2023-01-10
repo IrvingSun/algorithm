f6db4a4cb137527f3857da51a14d7bd3dfbbc95e
sunway <sunwei.pyw@gmail.com>
2023-01-10 14:45:49 +0800

    合并升序链表，递归方式

--- src/main/java/cn/sunway/algorithm/list/MergeTwoList.java
+++ src/main/java/cn/sunway/algorithm/list/MergeTwoList.java
@@ -12,6 +12,13 @@ import cn.sunway.structure.ListNode;
  */
 public class MergeTwoList {
 
+    /**
+     * 使用遍历的方式 构造虚拟节点，
+     * 然后 不断推动虚拟节点的next节点设置
+     * @param list1
+     * @param list2
+     * @return
+     */
     static ListNode merge(ListNode list1, ListNode list2) {
         ListNode temp = new ListNode();//虚拟节点
         ListNode current = temp;//将指针指向结果虚拟节点
@@ -30,6 +37,30 @@ public class MergeTwoList {
         return temp.next;
     }
 
+    /**
+     * 使用递归的方式
+     *
+     * 将两个链表头部较小的一个与剩下的元素合并，
+     * 并返回排好序的链表头，当两条链表中的一条为空时终止递归
+     *
+     * @param list1
+     * @param list2
+     * @return
+     */
+    static ListNode merge2(ListNode list1, ListNode list2){
+        if(list1 == null){
+            return list2;
+        }else if(list2 == null){
+            return list1;
+        }else if(list1.data < list2.data){
+            list1.next = merge2(list1.next, list2);
+            return list1;
+        }else{
+            list2.next = merge2(list1, list2.next);
+            return list2;
+        }
+    }
+
     public static void main(String[] args) {
         ListNode node1 = new ListNode(1);
         ListNode node2 = new ListNode(2);
@@ -43,6 +74,7 @@ public class MergeTwoList {
 
         node3.addTail(node6);
 
-        System.out.println(merge(node1, node3));
+//        System.out.println(merge(node1, node3));
+        System.out.println(merge2(node1, node3));
     }
 }
