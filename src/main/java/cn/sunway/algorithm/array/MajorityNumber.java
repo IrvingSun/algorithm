f2deeaa92cbf25d65ca82310a6cca13feb9994e0
sunway <sunwei.pyw@gmail.com>
2023-01-12 11:38:12 +0800

    出现次数最多(大于1/2)的数字

---
+++ src/main/java/cn/sunway/algorithm/array/MajorityNumber.java
@@ -0,0 +1,37 @@
+package cn.sunway.algorithm.array;
+
+/**
+ * 获取数组中出现次数最多的数字
+ *
+ * 投票计数法
+ *
+ * @author sunw
+ * @date 2023/1/12
+ */
+public class MajorityNumber {
+    private static int majority(int[] nums){
+        int current = nums[0]; //暂时无法删除的数字
+        int count = 0; //暂时无法删除的数字的数量
+
+        for(int i = 0; i < nums.length; i++){
+            if(count == 0){  //如果count==0，则需要替换无法删除的数字
+                current = nums[i];
+            }
+
+            if(current == nums[i]){
+                count ++;
+
+            }else{
+                count --;
+            }
+
+        }
+        return current;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(majority(new int[]{2,2,1,1,1,2,2,1,1}));
+        System.out.println(majority(new int[]{3,2,3}));
+    }
+
+}
